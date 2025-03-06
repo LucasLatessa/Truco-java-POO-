ba261deb82fac2eb2e209f0f795cd471ac45c906
lucaslatessa <lucaslatessa@gmail.com>
2025-03-06 16:27:34 -0300

    solo sin RMI

--- TrucoRMI/src/modelo/Palo.java
+++
@@ -1,7 +0,0 @@
-package modelo;
-
-import java.io.Serializable;
-
-public enum Palo implements Serializable {
-		ESPADA, BASTO,COPA,ORO
-}
--- TrucoSinRMI/src/observer/Observable.java
+++
@@ -1,7 +0,0 @@
-package observer;
-
-public interface Observable {
-	public void notificar(Object evento);
-	
-	public void agregarObservador(Observador observador);
-}
